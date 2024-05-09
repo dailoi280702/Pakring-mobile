ae016ca3df97de72d8e83ccbb848b39b438f88fd
Lloyd <lloyd@Lloyds-MacBook-Pro.local>
2024-05-09 13:55:34 +0700

    fix: create api client stead of returning get client function

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 import Config from "@src/config";
 
-const axiosClient = () => {
-  return axios.create({
-    baseURL: Config.API_BASE_URL,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
+const axiosClient = axios.create({
+  baseURL: Config.API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default axiosClient;
