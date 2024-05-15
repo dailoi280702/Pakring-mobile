8ad8ed81272b4079c734393acee0b652f4ad2c17
khoavo <khoavvd1709@gmail.com>
2024-05-15 10:10:28 +0700

    create change password screen

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import bookingSlice from "./slices/bookingSlice";
+import { userSlice } from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
+    user: userSlice.reducer,
     booking: bookingSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,3 +1,4 @@
 import { RootState } from ".";
 
 export const selectBooking = (state: RootState) => state.booking.entities;
+export const selectUser = (state: RootState) => state.user.data;
