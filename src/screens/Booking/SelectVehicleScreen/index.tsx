d46d96ed7aa5360fec9f574c9cb190e21506c26a
Lloyd <lloyd@Lloyds-MacBook-Pro.local>
2024-05-20 10:04:55 +0700

    get user vehicles

--- src/screens/Booking/SelectVehicleScreen/index.tsx
+++ src/screens/Booking/SelectVehicleScreen/index.tsx
@@ -1,9 +1,13 @@
+import { useAppSelector } from "@src/store/hooks";
+import { selectVehicles } from "@src/store/selectors";
 import { Text, View } from "react-native";
 
 const SelectVehicleScreen = ({ navigation }: any) => {
+  const vehicleState = useAppSelector(selectVehicles);
+
   return (
     <View style={{ flex: 1, justifyContent: "flex-start" }}>
-      <Text>Vehicl eScreen</Text>
+      <Text>{JSON.stringify(vehicleState)}</Text>
     </View>
   );
 };
