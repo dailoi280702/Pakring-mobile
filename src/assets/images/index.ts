683f848556b5d61b4a0b6d1c399f334593838230
Lloyd <lloyd@Lloyds-MacBook-Pro.local>
2024-05-20 15:54:42 +0700

    select payment method

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,6 +2,7 @@ const Images = {
   Car: require("./vehicles/car.png"),
   Bike: require("./vehicles/bike.png"),
   Van: require("./vehicles/van.png"),
+  Money: require("./money.png"),
 };
 
 export default Images;
--- src/screens/Booking/SelectPaymentScreen/index.tsx
+++ src/screens/Booking/SelectPaymentScreen/index.tsx
@@ -1,11 +1,46 @@
+import SelectablePaymentItem from "@src/components/Booking/SelectablePaymentItem";
 import AppButton from "@src/components/common/AppButton";
 import { Colors } from "@src/constants";
-import { StyleSheet, Text, View } from "react-native";
+import { ColorHelper } from "@src/utils";
+import { useState } from "react";
+import { Alert, FlatList, StyleSheet, Text, View } from "react-native";
 
 const SelectPaymentScreen = ({ navigation }: any) => {
+  const [selectedId, setSelectedId] = useState<string>(null);
+  const data = [
+    {
+      id: "1",
+      type: "Cash",
+    },
+  ];
+
+  const handleSelect = () => {
+    setSelectedId("1");
+  };
+
+  const navigateNext = () => {
+    if (selectedId) {
+      navigation.navigate("SummaryScreen");
+    } else {
+      Alert.alert("You must select payment method!");
+    }
+  };
   return (
     <View style={{ flex: 1, justifyContent: "flex-start" }}>
-      <Text>Choose payment method</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20 }}
+        renderItem={({ item }) => (
+          <SelectablePaymentItem
+            item={item}
+            checkedId={selectedId}
+            handleSelect={handleSelect}
+          />
+        )}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
       <AppButton style={styles.continueButton} onPress={navigateNext}>
         <Text style={styles.countinueText}>Countinue</Text>
       </AppButton>
@@ -16,6 +51,19 @@ const SelectPaymentScreen = ({ navigation }: any) => {
 export default SelectPaymentScreen;
 
 const styles = StyleSheet.create({
+  separator: {
+    height: 12,
+  },
+  addButton: {
+    backgroundColor: ColorHelper.hexToRgbA(Colors.light.primary, 0.2),
+    marginTop: 12,
+    marginBottom: 50,
+  },
+  addText: {
+    color: Colors.light.primary,
+    fontSize: 18,
+    fontWeight: "600",
+  },
   continueButton: {
     marginTop: 12,
     position: "absolute",
