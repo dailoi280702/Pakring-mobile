fef85d74dec9ba5c5aad96662ef89ace27e67863
khoavo <khoavvd1709@gmail.com>
2024-06-03 11:23:29 +0700

    add api display ongoing booking history

--- src/screens/BookingHistoryScreen/OngogingBookingScreen/index.tsx
+++ src/screens/BookingHistoryScreen/OngogingBookingScreen/index.tsx
@@ -1,35 +1,85 @@
-import { getTicketsOngoing } from "@src/store/actions/ticketAction";
+import { async } from "@firebase/util";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ticketApi, timeFrameApi } from "@src/api";
+import HistoryBookingItem from "@src/components/BookingHistory/HistoryBookingItem";
 import { useAppDispatch, useAppSelector } from "@src/store/hooks";
 import { selectOngoingTickets, selectUser } from "@src/store/selectors";
-import { useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { ticketActions } from "@src/store/slices/ticketSlice";
+import { timeFrameActions } from "@src/store/slices/timeFrameSlice";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { Alert, FlatList, View } from "react-native";
 
 type Props = {
   navigation: any;
 };
 
 const OngogingBookingScreen = ({ navigation }: Props) => {
-  const newTicketsState = useAppSelector(selectOngoingTickets);
+  const [isRefreshing, setRefreshing] = useState<boolean>(false);
   const userState = useAppSelector(selectUser);
+  const ongoingTicketState = useAppSelector(selectOngoingTickets);
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(getTicketsOngoing(userState.id));
-  }, []);
+
+  const onRefresh = () => {
+    (async () => {
+      let idUser;
+      if (userState?.id) {
+        idUser = await AsyncStorage.getItem("idUser");
+      }
+      setRefreshing(true);
+      dispatch(
+        ticketActions.getTicketsOngoing(userState?.id || idUser)
+      ).finally(() => setRefreshing(false));
+    })();
+    onExpire();
+  };
 
   const navigationTicket = (item: Ticket) => {
     navigation.navigate("BookingTicketScreen", item);
   };
 
+  useEffect(() => {
+    dispatch(ticketActions.getTicketsOngoing(userState?.id));
+  }, []);
+
+  const onExpire = () => {
+    ongoingTicketState.forEach(async (e) => {
+      if (dayjs(e.endTime).add(10, "minute").isBefore(new Date())) {
+        const resTimeFrame = await timeFrameApi.getAll(e.parkingLotId);
+        if (!resTimeFrame.data.data.data) {
+          Alert.alert("Faild when check expire ticket!");
+        }
+        const ticketExtendReq = {
+          ticketOriginId: e.id,
+          timeFrameId: resTimeFrame.data.data.data[0].id,
+          startTime: dayjs(e.endTime).utc().format(),
+          endTime: dayjs(e.endTime)
+            .add(resTimeFrame.data.data.data[0].duration, "minute")
+            .utc()
+            .format(),
+          total: resTimeFrame.data.data.data[0].cost,
+        };
+        const res = await ticketApi.extendTicket(ticketExtendReq);
+      }
+    });
+  };
+
+  useEffect(() => {
+    onExpire();
+  }, []);
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         style={{ flex: 1 }}
-        data={newTicketsState}
+        data={ongoingTicketState}
+        refreshing={isRefreshing}
+        onRefresh={onRefresh}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <Text style={{ margin: 12 }} onPress={() => navigationTicket(item)}>
-            {JSON.stringify(item)}
-          </Text>
+          <HistoryBookingItem
+            onViewTicket={() => navigationTicket(item)}
+            item={item}
+          />
         )}
         showsVerticalScrollIndicator={false}
       />
