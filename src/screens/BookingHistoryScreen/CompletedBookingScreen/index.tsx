695b9bde5d3ccb3d6d8dfb60623b49a484aceb43
khoavo <khoavvd1709@gmail.com>
2024-06-03 10:19:13 +0700

    create complete booking page

--- src/screens/BookingHistoryScreen/CompletedBookingScreen/index.tsx
+++ src/screens/BookingHistoryScreen/CompletedBookingScreen/index.tsx
@@ -1,35 +1,61 @@
-import { getTicketsCompleted  } from "@src/store/actions/ticketAction";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import HistoryBookingItem from "@src/components/BookingHistory/HistoryBookingItem";
 import { useAppDispatch, useAppSelector } from "@src/store/hooks";
 import { selectCompletedTickets, selectUser } from "@src/store/selectors";
-import { useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { ticketActions } from "@src/store/slices/ticketSlice";
+import { useEffect, useState } from "react";
+import { FlatList, View } from "react-native";
 
 type Props = {
   navigation: any;
 };
 
 const CompletedBookingScreen = ({ navigation }: Props) => {
-  const newTicketsState = useAppSelector(selectCompletedTickets);
+  const [isRefreshing, setRefreshing] = useState<boolean>(false);
   const userState = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(getTicketsCompleted(userState.id));
-  }, []);
+  const completedTicketState = useAppSelector(selectCompletedTickets);
+
+  const onRefresh = () => {
+    (async () => {
+      let idUser;
+      if (userState?.id) {
+        idUser = await AsyncStorage.getItem("idUser");
+      }
+      setRefreshing(true);
+      dispatch(
+        ticketActions.getTicketsCompleted(userState?.id || idUser)
+      ).finally(() => setRefreshing(false));
+    })();
+  };
 
   const navigationTicket = (item: Ticket) => {
     navigation.navigate("BookingTicketScreen", item);
   };
 
+  useEffect(() => {
+    (async () => {
+      let idUser;
+      if (userState?.id) {
+        idUser = await AsyncStorage.getItem("idUser");
+      }
+      dispatch(ticketActions.getTicketsCompleted(userState?.id || idUser));
+    })();
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         style={{ flex: 1 }}
-        data={newTicketsState}
+        data={completedTicketState}
+        refreshing={isRefreshing}
+        onRefresh={onRefresh}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <Text style={{ margin: 12 }} onPress={() => navigationTicket(item)}>
-            {JSON.stringify(item)}
-          </Text>
+          <HistoryBookingItem
+            onViewTicket={() => navigationTicket(item)}
+            item={item}
+          />
         )}
         showsVerticalScrollIndicator={false}
       />
